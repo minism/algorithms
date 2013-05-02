ed95c063b90258da3aac1bd2e57f13d2f1860df7
josh <jbothun@concentricsky.com>
2013-05-01 23:49:01 -0700

    fix find max subarray

--- find_max_subarray.py
+++ find_max_subarray.py
@@ -1,20 +1,24 @@
 import sys
 
 def find_max_subarray(L):
-	table = [(-sys.maxint, 0)]
-	for i in range(len(L)):
-		curr = sum(L[0:i+1])
-		if curr > table[0][0]:
-			table[0] = (curr, i)
+    best = None
+    left, right = 0, 0
+    total = 0
+    for i in range(len(L)):
+        total = total + L[i]
+        if not best or total > best:
+            best = total
+            right = i
+    total = best
+    for i in range(right):
+        total = total - L[i]
+        if total > best:
+            left = i+1
+            best = total
+    return L[left:right+1]
 
-	best = 0
-	for i in range(1, len(L)):
-		table.append((table[i-1][0] - L[i-1], table[i-1][1]))
-		if table[i][0] > table[best][0]:
-			best = i
 
 
 
-
-input = [10, 35, -5, -50, 40, 0]
+input = [40, 35, -5, -50, 60, 0]
 print find_max_subarray(input)
