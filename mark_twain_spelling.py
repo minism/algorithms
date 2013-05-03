3ed4e1940f011e8add55a4f96796cc8fc7629a24
Josh Bothun <joshbothun@gmail.com>
2013-05-02 22:19:50 -0700

    Update mark_twain_spelling.py

--- mark_twain_spelling.py
+++ mark_twain_spelling.py
@@ -2,30 +2,80 @@
 
 
 class Twain(object):
-
-    def year1(self, word):
-        return word 
-
-    def year2(self, word):
-        return word
-
-    def year3(self, word):
-        return word
-
-    def year4(self, word):
-        return word
-
-    def year5(self, word):
-        return word
-
-    def year6(self, word):
-        return word
-
-    def year7(self, word):
+    def year0(self, word):
         return ''.join(word)
 
+    def year1(self, word):
+        out = [word[0] == 'x' and 'z' or word[0]]
+        for char in word[1:]:
+            if char == 'x':
+                out.extend(['ks'])
+            else:
+                out.append(char)
+        return ''.join(out)
+
+    def year2(self, word):
+        return ''.join(word).replace('y', 'i')
+
+    def year3(self, word):
+        out = []
+        for i, char in enumerate(word):
+            if char == 'c' and i + 1 < len(word) and word[i+1] in ('e', 'i'):
+                out.append('s')
+            else:
+                out.append(char)
+        return ''.join(out)
+
+    def year4(self, word):
+        i = 0
+        while i < len(word):
+            char = word[i]
+            if char == 'k':
+                j = i - 1
+                while j >= 0 and word[j] == 'c':
+                    word[j] = None
+                    j -= 1
+            i += 1
+        return ''.join(filter(bool, word))
+
+    def year5(self, word):
+        out = []
+        i = 0
+        if len(word) > 2 and word[0] == 's' and word[1] == 'c' and word[2] == 'h':
+            out.extend(['sk'])
+            i = 3
+        while i < len(word):
+            char = word[i]
+            if char == 'c' and i + 2 < len(word) and word[i+1] == 'h' and word[i+2] == 'r':
+                out.append('k')
+                i += 2
+            else:
+                out.append(char) 
+                i += 1
+        for i, char in enumerate(out):
+            if char == 'c' and (i + 1 >= len(out) or out[i+1] != 'h'):
+                out[i] = 'k'
+        return ''.join(out)
+
+    def year6(self, word):
+        if len(word) > 1 and word[0] == 'k' and word[1] == 'n':
+            return ''.join(['n'] + word[2:])
+        return ''.join(word)
+
+    def year7(self, word):
+        i = 0
+        while i < len(word):
+            char = word[i]
+            if char not in ('a', 'e', 'i', 'o', 'u'):
+                j = i - 1
+                while j >= 0 and word[j] == char:
+                    word[j] = None
+                    j -= 1
+            i += 1
+        return ''.join(filter(bool, word))
+
     def getNewSpelling(self, year, phrase):
-        for i in range(1, min(year+1, 7+1)):
+        for i in range(min(year+1, 7+1)):
             func = getattr(self, "year%s" % i)
             words = map(lambda word: func(list(word)), phrase.split())
             phrase = ' '.join(words)
