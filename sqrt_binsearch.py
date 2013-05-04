1c48f838f361cf28ad57df952ab9daafb67c7699
josh <jbothun@concentricsky.com>
2013-05-04 06:13:56 -0700

    improve sqrt_binsearch

--- sqrt_binsearch.py
+++ sqrt_binsearch.py
@@ -4,14 +4,14 @@ def sqrt(n, error=0.1):
     lower = n < 1 and n or 1
     upper = n < 1 and 1 or n
     mid = lower + (upper - lower) / 2.0
-    square = mid ** 2
+    square = mid * mid
     while abs(square - n) > error:
         if square < n:
             lower = mid
         else:
             upper = mid
         mid = lower + (upper - lower) / 2.0
-        square = mid ** 2
+        square = mid * mid
     return mid
 
 print sqrt(0.9, error=0.00001)
